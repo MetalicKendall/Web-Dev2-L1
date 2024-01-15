a7e92faa45e3d99d1d2fd159bdd7cf596b6b0407
Kendall <kendalljosef@hotmail.com>
2024-01-14 22:43:34 -0800

    Lab 1 done

---
+++ script.js
@@ -0,0 +1,79 @@
+//Task 1
+//let firstName;
+//let lastName;
+//firstName= "Kendall";
+//lastName="Josef";
+//console.log(`Hello, ${firstName} ${lastName}!`);
+
+
+//Task 2
+//let globalVar = 10;
+//console.log(globalVar);
+
+//    if(true){
+//        let localVar=20;
+//        console.log(localVar);
+//    }
+    //localVar is not defined outside of the if statement
+    //console.log(localVar);
+
+
+//Task 3
+//let num1 = 1;
+//let Even = (num1 % 2 == 0);
+//let isEven = Even ? `${num1} is even` : `${num1} is odd`;
+//console.log(isEven);
+
+
+//Task 4
+//let fruit1 = "apple";
+//let fruit2 = "banana";
+//let fruitMessage = fruit1 + fruit2;
+//console.log(fruitMessage);
+
+
+//Task 5
+//let userName = "Kendall";
+//let isLoggedIn = true;
+//let userLogged = isLoggedIn ? `Welcome back, ${userName}!` : `Hello, ${userName}! Please sign in`;
+//console.log(userLogged);
+
+
+//Task 6
+//let globalVar = 10; 
+//if(true){
+//    let localVar = 5;
+//    console.log(localVar);
+    //can print globalVar inside of block
+//    console.log(globalVar);
+
+//}
+//cannot print localVar outside of block
+//console.log(localVar);
+//console.log(globalVar);
+
+
+//Task 7
+//let firstName = "Kendall";
+//let lastName = "Josef";
+//let fullName =  firstName + " " + lastName;
+//console.log(fullName);
+
+
+//Task 8
+//let product = "chips";
+//let discount = 20;
+//let isDiscount = discount > 0 ? `${discount}% discount on ${product}` : `No discount on ${product}`;
+//console.log(isDiscount);
+
+
+//Task 9
+//let name1 = "Bob";
+//let place = "Route 66";
+//let activity ="shootin" ;
+//console.log(`Once upon a time, ${name1} visted ${place} and enjoyed ${activity}.`)
+
+//Task 10
+//Html is how the webpage is strutcured
+//CSS is how the webpage is styled
+//JavaScript is data that the page can use. like user data or input
